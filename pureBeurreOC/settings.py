9b1903d279dfd620a8f6691bf47ab7115531065c
Erwan <morioe2809@gmail.com>
2020-06-08 11:08:54 +0200

    update settings

--- pureBeurreOC/settings.py
+++ pureBeurreOC/settings.py
@@ -121,8 +121,8 @@ STATIC_URL = "/static/"
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, "static"),
     os.path.join(BASE_DIR, "products/static"),
+    os.path.join(PROJECT_ROOT, "static"),
 )
 
 # Simplified static file serving.
