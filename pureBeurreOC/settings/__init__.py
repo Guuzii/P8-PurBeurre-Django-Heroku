ba37bdfb5f47c6f368334bf4d52fdc6203c0dc8d
Erwan <morioe2809@gmail.com>
2020-07-20 17:37:21 +0200

    modification settings pour fix static - 3

--- pureBeurreOC/settings/__init__.py
+++ pureBeurreOC/settings/__init__.py
@@ -124,9 +124,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 # Static files settings
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
