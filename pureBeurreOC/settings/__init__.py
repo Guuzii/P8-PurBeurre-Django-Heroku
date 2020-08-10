ecdf9bf7fed3a6f9e8683b98533b65ad49b8b03b
Erwan <morioe2809@gmail.com>
2020-08-10 10:34:48 +0200

    add travis settings

--- pureBeurreOC/settings/__init__.py
+++ pureBeurreOC/settings/__init__.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = "pureBeurreOC.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "purbeurre",
+        "NAME": "PurBeurre",
         "USER": "postgres",
         "PASSWORD": "",
         "HOST": "",
