0957b1989bbcd00e5fcfc28501f7236557ac3b92
Erwan <morioe2809@gmail.com>
2020-10-09 12:49:28 +0200

    delete var in settings

--- pureBeurreOC/settings/__init__.py
+++ pureBeurreOC/settings/__init__.py
@@ -129,9 +129,9 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "products/static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "products/static"),
+# )
 
 # Django crontab
 CRONJOBS = [
